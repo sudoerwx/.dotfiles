feaf8e9c5e594c29421dbebcef20237b32a1c1ae
Nazar <voronych@merck.com>
2025-03-19 09:51:48 -0400

    aeoueoueoueou

--- stow/nvim/dot-config/nvim/lua/core/lazy/telescope.lua
+++ stow/nvim/dot-config/nvim/lua/core/lazy/telescope.lua
@@ -1,11 +1,23 @@
 return {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup {
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            }
+        }
+        require('telescope').load_extension('fzf')
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files({ hidden = true }) end, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
