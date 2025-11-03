de1066285e3a3b232b822c9f1ae206c7cdc6cfdb
sudoerwx <nazarvoronych@gmail.com>
2025-11-03 12:06:36 +0000

    oeuaoeu

--- bin/.config/nvim/lua/core/lazy/peek.lua
+++ bin/.config/nvim/lua/core/lazy/peek.lua
@@ -5,7 +5,8 @@ return {
         build = "deno task --quiet build:fast",
         config = function()
             require("peek").setup({
-                filetype = { 'markdown', 'conf' }
+                filetype = { 'markdown', 'conf' },
+                app = 'browser'
             })
             vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
             vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
