186eb0b7ed52b0e25b5cc0b1d7feac828d8441c5
sudoerwx <nazarvoronych@gmail.com>
2026-02-06 13:40:22 +0200

    oeuoa

--- bin/.config/nvim/lua/core/lazy/avante.lua
+++ bin/.config/nvim/lua/core/lazy/avante.lua
@@ -14,27 +14,13 @@ return { {
         -- this file can contain specific instructions for your project
         instructions_file = "avante.md",
         -- for example
+        mode = "legacy",
         provider = "copilot",
-        -- providers = {
-        --     claude = {
-        --         endpoint = "https://api.anthropic.com",
-        --         model = "claude-sonnet-4-20250514",
-        --         timeout = 30000, -- Timeout in milliseconds
-        --         extra_request_body = {
-        --             temperature = 0.75,
-        --             max_tokens = 20480,
-        --         },
-        --     },
-        --     moonshot = {
-        --         endpoint = "https://api.moonshot.ai/v1",
-        --         model = "kimi-k2-0711-preview",
-        --         timeout = 30000, -- Timeout in milliseconds
-        --         extra_request_body = {
-        --             temperature = 0.75,
-        --             max_tokens = 32768,
-        --         },
-        --     },
-        -- },
+        providers = {
+            -- copilot = {
+            --     model = "gpt-5.2",
+            -- },
+        },
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
