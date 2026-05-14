6c1fa97d69314feb944ca7a5af189c1e0e00a5ee
sudoerwx <nazarvoronych@gmail.com>
2026-05-14 07:12:44 +0000

    oeuaoeu

--- bin/.config/nvim/lua/core/lazy/avante.lua
+++ bin/.config/nvim/lua/core/lazy/avante.lua
@@ -22,7 +22,8 @@ return { {
         -- mode = "agentic",
         providers = {
             copilot = {
-                model = "gpt-4.1",
+                -- model = "gpt-4.1",
+                model = "gpt-5-mini",
             },
             -- codex = {}
         },
@@ -59,13 +60,13 @@ return { {
                 },
             },
         },
-        {
-            -- Make sure to set this up properly if you have lazy=true
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { "markdown", "Avante" },
-            },
-            ft = { "markdown", "Avante" },
-        },
+        -- {
+        --     -- Make sure to set this up properly if you have lazy=true
+        --     'MeanderingProgrammer/render-markdown.nvim',
+        --     opts = {
+        --         file_types = { "markdown", "Avante" },
+        --     },
+        --     ft = { "markdown", "Avante" },
+        -- },
     },
 } }
