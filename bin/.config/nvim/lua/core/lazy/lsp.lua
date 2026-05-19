68e25934f4671b899d6203d36f94745c9c5ca6fd
sudoerwx <nazarvoronych@gmail.com>
2026-05-19 14:06:57 +0300

    oaeuaoue

--- bin/.config/nvim/lua/core/lazy/lsp.lua
+++ bin/.config/nvim/lua/core/lazy/lsp.lua
@@ -4,19 +4,11 @@ return {
         "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
         "j-hui/fidget.nvim",
     },
 
     config = function()
-        local cmp = require('cmp')
 
         require("fidget").setup({
             notification = {      -- NOTE: you're missing this outer table
@@ -75,36 +67,6 @@ return {
             }
 
         })
-
-
-
-
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'path' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-                { name = 'luasnip' }, -- For luasnip users.
-            })
-        })
-
 
         vim.diagnostic.config({
             virtual_text = {
